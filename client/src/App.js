18f5a03e8f4d6159f93895db05c546a4e2e13b9c
yash Prajapati <prajapatiyash23@hotmail.com>
2023-03-30 20:03:44 -0500

    added circles component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import AssignSection from './components/Assign'
+import AssignSection from './components/Assign';
+import OwnerVisualization from './components/OwnerVisualization';
 
 function App() {
   return (
     <div>
-      
+      <OwnerVisualization/>
       
       <AssignSection />
     </div>
---
+++ client/src/components/OwnerVisualization.jsx
@@ -0,0 +1,10 @@
+function OwnerVisualization(){
+    return(
+        <>
+        //create 5 circles or shapes representing the 5 OwnerVisualization
+        <div>5 circles</div> 
+        </>
+    );
+}
+
+export default OwnerVisualization;
